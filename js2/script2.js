3dc094df641926f646295fd533a92bb00a5ab436
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-21 19:13:34 +0100

    [UPD] inserite le scelte dell'utente

--- js2/script2.js
+++ js2/script2.js
@@ -2,4 +2,13 @@
 
 // chiedo all'utente di scegliere pari o dispari
 
-let sceltaUtente = prompt("Scegli PARI o DISPARI")
+let sceltaUtente = prompt("Scegli PARI o DISPARI");
+console.log(sceltaUtente);
+
+// chiedo all'utente di scegliere un numero da 1 a 5
+
+let sceltaNumeroUtente = prompt("Scegli un numero da 1 a 5");
+console.log(sceltaNumeroUtente);
+
+// genero un numero random da 1 a 5 per il computer tramite una funzione
+// 
