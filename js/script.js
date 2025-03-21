dea39cfd0bde09901b7e677ffa0c41082dbad4ff
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-21 16:15:15 +0100

    [UPD] invertito l'ordine della parola inserita

--- js/script.js
+++ js/script.js
@@ -4,11 +4,14 @@
 // dichiaro una variabile
 
 let userWord = prompt('Inserisci la parola che vuoi analizzare');
+let userWordReversed = [];
 console.log(userWord);
 
 // verifico se la parola inserita è palindroma
 // dichiaro una funzione
 
-function verificationWord(){
-    
-}
+//function verificationWord(){
+    userWordReversed = userWord.split("")
+    userWordReversed.reverse()
+    console.log(userWordReversed);
+
