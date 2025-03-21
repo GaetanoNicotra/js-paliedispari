77a717e00dbe153a0140920e57aff002575a789f
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-21 17:07:26 +0100

    [UPD]

--- js/script.js
+++ js/script.js
@@ -4,21 +4,25 @@
 // dichiaro una variabile
 
 let userWord = prompt('Inserisci la parola che vuoi analizzare');
+let userWord2 = userWord.split("")
+userWord2 = userWord2.toString()
 let userWordReversed = [];
-console.log(userWord);
+console.log(userWord2);
 
 // verifico se la parola inserita è palindroma
 // dichiaro una funzione
 
 //function verificationWord() {
-    userWordReversed = userWord.split("")
-    userWordReversed.reverse()
-    console.log(userWordReversed);
 
-if(userWord == userWordReversed){
+userWordReversed = userWord.split("")
+userWordReversed.reverse()
+userWordReversed = userWordReversed.toString()
+console.log(userWordReversed);
+
+if (userWord2 == userWordReversed) {
     console.log("La parola è PALINDROMA")
 
 }
- else{
+else {
     console.log("La parola non è PALINDROMA")
- }
+}
