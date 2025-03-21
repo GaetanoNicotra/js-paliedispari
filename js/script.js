996132b9f5359b4b3efb2c28d88c0c72214e0892
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-21 14:52:10 +0100

    [UPD] Creazione dei file di lavoro e inizio svolgimento del primo esercizio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// PRIMO ESERCIZIO: PALINDROMA
+
+// chiedo all'utente di inserire una parola
+// dichiaro una variabile
+
+let userWord = prompt('Inserisci la parola che vuoi analizzare');
+console.log(userWord);
