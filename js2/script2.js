c1b33df7c58269b9f63f49c66f67891347731390
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-21 20:57:50 +0100

    [UPD] scritta la funzione per generare un numero random

--- js2/script2.js
+++ js2/script2.js
@@ -2,13 +2,21 @@
 
 // chiedo all'utente di scegliere pari o dispari
 
-let sceltaUtente = prompt("Scegli PARI o DISPARI");
-console.log(sceltaUtente);
+let sceltaUtente = prompt("Scegli Pari o Dispari");
+console.log('Hai scelto' + ' ' + sceltaUtente);
 
 // chiedo all'utente di scegliere un numero da 1 a 5
 
 let sceltaNumeroUtente = prompt("Scegli un numero da 1 a 5");
-console.log(sceltaNumeroUtente);
+console.log('Il tuo numero è' + ' ' + sceltaNumeroUtente);
 
 // genero un numero random da 1 a 5 per il computer tramite una funzione
-// 
+// dichiaro la funzione
+
+function randomNumber (){
+    valoreGenerato = Math.floor(Math.random() * 5) + 1;
+    console.log('Il numero del computer è'+  ' ' + valoreGenerato )
+    return valoreGenerato;
+}
+
+let risultatoRandom = randomNumber()
