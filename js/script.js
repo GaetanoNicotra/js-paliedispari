c1c982124cbe25d55c51193f0dd8a124c36bb55b
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-21 18:43:09 +0100

    [UPD] sistemato e ordinato il codice

--- js/script.js
+++ js/script.js
@@ -4,23 +4,30 @@
 // dichiaro una variabile
 
 let userWord = prompt('Inserisci la parola che vuoi analizzare');
-let userWordReversed = [];
+
 console.log(userWord);
 
+// richiamo la funzione
+
+let parolaGirata = verificationWord(userWord);
+
 // verifico se la parola inserita è palindroma
-// dichiaro una funzione
 
-//function verificationWord() {
-
-userWordReversed = userWord.split("")
-userWordReversed.reverse()
-userWordReversed = userWordReversed.join("")
-console.log(userWordReversed);
-
-if (userWord == userWordReversed) {
+if (userWord == parolaGirata) {
     console.log("La parola è PALINDROMA")
 
 }
 else {
     console.log("La parola non è PALINDROMA")
+}
+
+// dichiaro la funzione per la verifica della parola
+
+function verificationWord(word) {
+    let userWordReversed = [];
+    userWordReversed = word.split("")
+    userWordReversed.reverse()
+    userWordReversed = userWordReversed.join("")
+    console.log(userWordReversed);
+    return userWordReversed;
 }
