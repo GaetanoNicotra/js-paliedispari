33bdc433a596735b2942eb82fa0a1c512fe8df83
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-21 21:44:32 +0100

    [UPD] Creazione della funzione per la somma

--- js2/script2.js
+++ js2/script2.js
@@ -7,16 +7,50 @@ console.log('Hai scelto' + ' ' + sceltaUtente);
 
 // chiedo all'utente di scegliere un numero da 1 a 5
 
-let sceltaNumeroUtente = prompt("Scegli un numero da 1 a 5");
+let sceltaNumeroUtente = parseInt(prompt("Scegli un numero da 1 a 5"));
 console.log('Il tuo numero è' + ' ' + sceltaNumeroUtente);
 
-// genero un numero random da 1 a 5 per il computer tramite una funzione
+// stampo il numero random richiamando la funzione creata in precedenza
+
+let risultatoRandom = randomNumber()
+
+// verifico se il numero inserito dall'utente è compreso tra 1 e 5
+// creo un ciclo for
+//for(;sceltaUtente < 1 || sceltaUtente > 6;){
+//sceltaUtente = parseInt(prompt('ERRORE: Hai inserito zero o un numero maggiore di 6'))
+
+// controllo la somma e restituisco l'esito della giocata
+
+let risultatoSomma = controllo(sceltaNumeroUtente, risultatoRandom)
+console.log('La somma dei numeri è = ' + ' ' +risultatoSomma )
+
+if (sceltaUtente == sum) {
+    console.log('Hai Vinto')
+}
+else {
+    console.log('Hai perso')
+}
+
+
+
+// *******  FUNZIONI  *******
+
+// funzione per generare un numero random da 1 a 5 
 // dichiaro la funzione
 
-function randomNumber (){
+function randomNumber() {
     valoreGenerato = Math.floor(Math.random() * 5) + 1;
-    console.log('Il numero del computer è'+  ' ' + valoreGenerato )
+    console.log('Il numero del computer è' + ' ' + valoreGenerato)
     return valoreGenerato;
 }
 
-let risultatoRandom = randomNumber()
+function controllo(a, b,) {
+    sum = a + b;
+    if ((a + b) % 3 == 0) {
+        
+    }
+    else if ((a + b) % 2 == 0) {
+        
+    }
+    return sum;
+}
